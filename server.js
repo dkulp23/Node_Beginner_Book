fd58eab2d53c8b931b3a4305a7e8806ac4d7f414
Dana Kulp <dkulp23@gmail.com>
2017-02-25 21:11:04 -0800

    Async test.

--- server.js
+++ server.js
@@ -3,9 +3,12 @@
 const http = require('http');
 
 function onRequest(req, res) {
+	console.log('Request Received');
 	res.writeHead(200, { 'Content-Type': 'text/plain'});
 	res.write('hello world');
 	res.end();	
 };
 
 http.createServer(onRequest).listen(3000);
+
+console.log('Server has started');
