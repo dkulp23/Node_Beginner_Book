061dc02fdd03ab738dd6fc979fcb78892896fab7
Dana Kulp <dkulp23@gmail.com>
2017-02-25 23:19:24 -0800

    HTML form added.

--- requestHandlers.js
+++ requestHandlers.js
@@ -5,15 +5,22 @@ const exec = require('child_process').exec;
 function start(res) {
 	console.log('Request handler "start" was called.');
 
-	const content = 'empty';
+	const body = '<html>' +
+	'<head>' +
+	'<meta http-equiv="Content-Type" content="text/html; ' +
+	'charset=UTF-8" />' +
+	'</head>' +
+	'<body>' +
+	'<form action="/upload" method="post">' +
+	'<textarea name="text" rows="20" cols="60"></textarea>' +
+	'<input type="submit" value="Submit text" />' +
+	'</form>' +
+	'</body>' +
+	'</html>';
 
-	exec('ls -lah', function(error, stdout, stderr) {
-		res.writeHead(200, { 'Content-Type': 'text/plain'});
-		res.write(stdout);
-		res.end();
-	});
-
-	return content;
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(body);
+	res.end();
 };
 
 function upload(res) {
